2508707f9229d0f3e43ef121bc8653f837503142
Kristea Gamel <krgamel@gmail.com>
2019-02-27 19:58:09 -0600

    began setting up componend in header and imports to header and footer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return ( 
+        <div className = "HeaderTitle">
+        <p > HeaderTitle </p> </div>
+    );
+};
+
+export default HeaderTitle;
