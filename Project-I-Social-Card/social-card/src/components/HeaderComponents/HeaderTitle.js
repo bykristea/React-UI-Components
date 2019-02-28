f1019dc106b541d840b4c04b6adcaf77e151e8ee
Kristea Gamel <krgamel@gmail.com>
2019-02-27 21:25:26 -0600

    added in moment.js
    to diplay the date on the actual day you are looking at it

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import moment from 'moment';
 import './Header.css';
 
-const HeaderTitle = () => {
+class HeaderTitle extends React.Component {
+    render() {
     return ( 
         <div className = "HeaderTitle">
             <h1>Lambda School </h1> 
             <h3>@LambdaSchool </h3> 
-            <h3>26 jan </h3>
+            <h3>•</h3>
+            <h3>{moment().format("D MMM")}</h3>
         
         </div>
     );
+  }
 };
 
 export default HeaderTitle;
