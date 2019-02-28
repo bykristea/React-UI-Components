b15200d6acf89d59707c9308aa6dee1209d0615a
Kristea Gamel <krgamel@gmail.com>
2019-02-27 22:13:56 -0600

    got calculator to show up
    styled buttons and used props to get the visual there

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <div className="CalculatorDisplay">
+            0
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
